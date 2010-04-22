cf24553f7cfb53fc797c6b9fa5be84d246a0dcc5
Maximilian Schulz <max@jungeelite.de>
2010-04-22 23:32:27 +0800

    Make access token expiration date optional for offline_access privilege usage

--- lib/rfgraph/auth.rb
+++ lib/rfgraph/auth.rb
@@ -16,10 +16,18 @@ module RFGraph
     def authorize_url(callback_url)
       "#{BASE_URL}/oauth/authorize?client_id=#{app_id}&redirect_uri=#{callback_url}"
     end
-
+    
+    # Take the oauth2 request token and turn it into an access token 
+    # which can be used to access private data
     def authorize(callback_url, code)
       data = open("#{BASE_URL}/oauth/access_token?client_id=#{app_id}&redirect_uri=#{CGI.escape callback_url}&client_secret=#{app_secret}&code=#{CGI.escape code}").read
-      @expires = data.match(/expires=([^&]+)/)[1]
+      
+      # The expiration date is not necessarily set, as the app might have
+      # requested offline_access to the data
+      match_data = data.match(/expires=([^&]+)/)
+      @expires = match_data && match_data[1] || nil
+      
+      # Extract the access token
       @access_token = data.match(/access_token=([^&]+)/)[1]
     end
   end
